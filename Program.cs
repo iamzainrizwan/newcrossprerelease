cae26f2a2f9d486eeca33b11199f264a5572f0d9
iamzainrizwan <iamzainrizwan@gmail.com>
2024-10-21 14:52:51 +0100

    help

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace newcrossprerelease
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
